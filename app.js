6ebfcf9f1ad08e684018863b5052d976770e2f62
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-19 07:50:24 +0430

    adding some functionality

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const btns = document.querySelectorAll(".question-btn");
 
 btns.forEach(function(btn){
     btn.addEventListener("click", function(e){
-
+        console.log(e.currentTarget);
     });
 });
