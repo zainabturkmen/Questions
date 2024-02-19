7371cdc9944264377010bc996e6adcca7ffcd86c
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-19 08:20:08 +0430

    adding some functionality

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 //using selectors inside the element
+
+
+
+
+
+
+
+
+
+
 // traversing the dom
 
-const btns = document.querySelectorAll(".question-btn");
+// const btns = document.querySelectorAll(".question-btn");
 
-btns.forEach(function(btn){
-    btn.addEventListener("click", function(e){
-        console.log(e.currentTarget);
-    });
-});
+// btns.forEach(function(btn){
+//     btn.addEventListener("click", function(e){
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle("show-text")
+//     });
+// });
